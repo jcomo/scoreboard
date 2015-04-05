6ed4ca032c836ba04dadbda7bc2b2e393fd42be1
Jonathan Como <jonathan.como@gmail.com>
2015-04-05 13:37:14 -0700

    finish up command line program

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 	"time"
 )
 
-func main() {
-	sb := NewScoreboard(DefaultHttpClient())
+func printCurrentTeamStatus(sb *Scoreboard, team string) {
+	game, err := sb.GetTeam(time.Now(), team)
+	if err != nil {
+		fmt.Println("ERROR!")
+	} else {
+		fmt.Println(game)
+	}
+}
+
+func printCurrentStatus(sb *Scoreboard) {
 	games, err := sb.Get(time.Now())
 	if err != nil {
 		fmt.Println("ERROR!")
@@ -14,5 +24,18 @@ func main() {
 		for _, g := range games {
 			fmt.Println(g)
 		}
+	}
+}
+
+func main() {
+	teamFlag := flag.String("team", "", "Team to focus on (eg. NYY)")
+	flag.Parse()
+
+	sb := NewScoreboard(DefaultHttpClient())
+	team := strings.ToUpper(*teamFlag)
+	if team != "" {
+		printCurrentTeamStatus(sb, team)
+	} else {
+		printCurrentStatus(sb)
 	}
 }
