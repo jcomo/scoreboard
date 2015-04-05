6b5b629f58bf3a3d5d722e4b9d9ad00e02f0cdea
Jonathan Como <jonathan.como@gmail.com>
2015-04-05 12:59:13 -0700

    clean up client test

--- client_test.go
+++ client_test.go
@@ -2,46 +2,21 @@ package main
 
 import (
 	"testing"
-	"time"
 )
 
 func TestHttpClient(t *testing.T) {
-	// Spot check using the data from 4/4/2015
-	// There were 15 games played on that day
-	// One game result was WSH 3 - 4 NYY
-	d := time.Date(2015, 4, 4, 0, 0, 0, 0, time.Local)
-
-	c := DefaultHttpClient()
-	games, err := c.FetchGames(d)
-
-	assertNoError(t, err)
-	assertEqual(t, 15, len(games))
-
-	want := FinishedGame{
-		home: newTeamStatus("WSH", 3),
-		away: newTeamStatus("NYY", 4),
-	}
-
-	found := false
-	for _, game := range games {
-		if game.State() == want.State() {
-			found = true
-		}
-	}
-
-	if !found {
-		t.Errorf("Did not find expected entry for game: %s", want.State())
-	}
+	assertGamesOnTestDate(t, DefaultHttpClient())
 }
 
 func TestFixtureClient(t *testing.T) {
+	assertGamesOnTestDate(t, DefaultFixtureClient())
+}
+
+func assertGamesOnTestDate(t *testing.T, c Client) {
 	// Spot check using the data from 4/4/2015
 	// There were 15 games played on that day
 	// One game result was WSH 3 - 4 NYY
-	d := time.Date(2015, 4, 4, 0, 0, 0, 0, time.Local)
-
-	c := DefaultFixtureClient()
-	games, err := c.FetchGames(d)
+	games, err := c.FetchGames(testDate)
 
 	assertNoError(t, err)
 	assertEqual(t, 15, len(games))
